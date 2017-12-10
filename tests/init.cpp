ebe05a9e1ad572af981e7900980395f22ddadcff
Bozey98 <dmusatov98@gmail.com>
2017-12-11 02:30:14 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -9,3 +9,28 @@ TEST_CASE("insert_root", "[root]") {
 	REQUIRE(*test.getLevel(6) == 1);
 		
 }
+/*
+
+              6
+	    /	\
+	  2	  8
+*/
+
+TEST_CASE("insert2", "[ins2]") {
+	AATree<int> test;
+	test.insert(6);
+	test.insert(2);
+	test.insert(8);
+	REQUIRE(*test.getKeyRoot() == 6);
+	REQUIRE(test.getcount() == 3);
+	REQUIRE(test.getroot() != 0);
+	REQUIRE(*test.getLevel(6) == 2);
+	REQUIRE(*test.getLevel(2) == 1);
+	REQUIRE(*test.getLevel(8) == 1);
+	REQUIRE(*test.getLeftKey(6) == 2);
+        REQUIRE(*test.getRightKey(6) == 8);
+	REQUIRE(*test.getLeftKey(2) == 2);
+        REQUIRE(*test.getRightKey(2) == 2);
+	REQUIRE(*test.getLeftKey(8) == 8);
+        REQUIRE(*test.getRightKey(8) == 8);
+}
