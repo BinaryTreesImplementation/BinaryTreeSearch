248ad14151846b4dfc26369a929c924cced8a1f4
Bozey98 <dmusatov98@gmail.com>
2017-12-11 02:41:28 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -34,3 +34,39 @@ TEST_CASE("insert2", "[ins2]") {
 	REQUIRE(*test.getLeftKey(8) == 8);
         REQUIRE(*test.getRightKey(8) == 8);
 }
+
+
+/*
+
+              6
+	    /	\						6 ------ 10   (the same level)
+	   /     \					       /        /  \
+	  2	  8  --- 16 			-->>          2        8    16 
+                         /  (the same level)  
+			10  
+*/
+
+TEST_CASE("insert3", "[ins3]") {
+	AATree<int> test;
+	test.insert(6);
+	test.insert(2);
+	test.insert(8);
+	REQUIRE(*test.getKeyRoot() == 6);
+	REQUIRE(test.getcount() == 3);
+	REQUIRE(test.getroot() != 0);
+	REQUIRE(*test.getLevel(6) == 2);
+	REQUIRE(*test.getLevel(10) == 2);
+	REQUIRE(*test.getLevel(2) == 1);
+	REQUIRE(*test.getLevel(8) == 1);
+	REQUIRE(*test.getLevel(16) == 1);
+	REQUIRE(*test.getLeftKey(6) == 2);
+        REQUIRE(*test.getRightKey(6) == 10);
+	REQUIRE(*test.getLeftKey(10) == 8);
+        REQUIRE(*test.getRightKey(10) == 16);
+	REQUIRE(*test.getLeftKey(2) == 2);
+        REQUIRE(*test.getRightKey(2) == 2);
+	REQUIRE(*test.getLeftKey(8) == 8);
+        REQUIRE(*test.getRightKey(8) == 8);
+	REQUIRE(*test.getLeftKey(16) == 16);
+        REQUIRE(*test.getRightKey(16) == 16);
+}
