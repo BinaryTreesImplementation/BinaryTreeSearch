114e45a420845b7c643c9b09273add4ea5c3bcdd
Bozey98 <dmusatov98@gmail.com>
2017-12-11 03:50:28 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -121,5 +121,40 @@ TEST_CASE("remove root", "[root]") {
    test.insert(15);
    test.remove(15);      
    REQUIRE(test.getcount() == 0);
-   REQUIRE(test.getroot() == 0);
 }
+
+/*
+      6                         
+    /   \      (-2) ----->      6 -> 8 
+   2     8   
+
+*/
+TEST_CASE("remove_1", "[r1]") {
+   AATree<int> test;
+   test.insert(6);
+   test.insert(2);  
+   test.insert(8);
+   REQUIRE(test.getcount() == 3);
+   REQUIRE(*test.getLevel(6) == 2);
+   REQUIRE(*test.getLevel(2) == 1);
+   REQUIRE(*test.getLevel(8) == 1);
+   REQUIRE(*test.getLeftKey(6) == 2);
+   REQUIRE(*test.getRightKey(6) == 8);
+   REQUIRE(*test.getLeftKey(2) == 2);
+   REQUIRE(*test.getRightKey(2) == 2);
+   REQUIRE(*test.getLeftKey(8) == 8);
+   REQUIRE(*test.getRightKey(8) == 8);
+   
+   test.remove(2);
+   REQUIRE(test.getcount() == 2);
+   REQUIRE(*test.getLevel(6) == 1);
+   REQUIRE(*test.getLevel(2) == 0);
+   REQUIRE(*test.getLevel(8) == 1);
+   REQUIRE(*test.getLeftKey(6) == 6);
+   REQUIRE(*test.getRightKey(6) == 8);
+   REQUIRE(*test.getLeftKey(8) == 8);
+   REQUIRE(*test.getRightKey(8) == 8);
+}
+
+
+
