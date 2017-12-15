e52b19978728075850760e339aa8d8cccbce41a3
Bozey98 <dmusatov98@gmail.com>
2017-12-15 13:23:11 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,3 +12,29 @@ TEST_CASE("insert_root", "[root]") {
    REQUIRE(test.getParentKey(5) == nullptr);
    REQUIRE(test.get_count() == 1);
 }
+
+/* insert left child
+  
+       5        ---+3---->      5  
+                               /
+                              3
+
+
+
+*/
+
+TEST_CASE("insert_root", "[root]") {
+  
+  BST<int> test;
+  
+  test.insert(5);
+  test.insert(3);
+   REQUIRE(*test.getKeyRoot() == 5);
+   REQUIRE(*test.getLeftKey(5) == 3);
+   REQUIRE(test.getRightKey(5) == nullptr);
+   REQUIRE(test.getParentKey(5) == nullptr);
+   REQUIRE(*test.getParentKey(3) == 5);
+   REQUIRE(test.getLeftKey(3) == nullptr);
+   REQUIRE(test.getRightKey(3) == nullptr);
+   REQUIRE(test.get_count() == 2);
+}
