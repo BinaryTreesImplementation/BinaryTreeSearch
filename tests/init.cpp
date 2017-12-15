66b32adafb983dd7c980e80e0247bf33bb1c77eb
Bozey98 <dmusatov98@gmail.com>
2017-12-15 14:00:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -74,7 +74,7 @@ TEST_CASE("insert_rightCh", "[insRight]") {
 
 
 
-/* insert left child
+/* insert left and right children
   
        5        ---+3 +7---->       5  
                                    /  \
@@ -103,6 +103,43 @@ TEST_CASE("insert_2_LR_child", "[ins2]") {
    REQUIRE(*test.getParentKey(3) == 5);
    REQUIRE(test.getLeftKey(3) == nullptr);
    REQUIRE(test.getRightKey(3) == nullptr);
+  
+   REQUIRE(test.get_count() == 3);
+}
+
+
+
+/* insert left child with left parent
+  
+       5        ---+3 +7---->       5  
+                                   /  
+                                 3      
+                                /
+                               1
+
+
+
+*/
+
+TEST_CASE("insert_left_left", "[insLL]") {
+  
+  BST<int> test;
+  
+  test.insert(5);
+  test.insert(3);
+  test.insert(1);
+   REQUIRE(*test.getKeyRoot() == 5);
+   REQUIRE(*test.getLeftKey(5) == 3);
+   REQUIRE(*test.getRightKey(5) == 7);
+   REQUIRE(test.getParentKey(5) == nullptr);
+  
+   REQUIRE(*test.getParentKey(3) == 5);
+   REQUIRE(test.getLeftKey(3) == 1);
+   REQUIRE(test.getRightKey(3) == nullptr);
+  
+   REQUIRE(*test.getParentKey(1) == 3);
+   REQUIRE(test.getLeftKey(1) == nullptr);
+   REQUIRE(test.getRightKey(1) == nullptr);
   
    REQUIRE(test.get_count() == 3);
 }
