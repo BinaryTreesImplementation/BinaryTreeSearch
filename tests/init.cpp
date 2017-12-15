8b68c68168e230bbd1e5f652aab155c6c6cd282b
Bozey98 <dmusatov98@gmail.com>
2017-12-15 13:50:27 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -10,6 +10,7 @@ TEST_CASE("insert_root", "[root]") {
    REQUIRE(test.getLeftKey(5) == nullptr);
    REQUIRE(test.getRightKey(5) == nullptr);
    REQUIRE(test.getParentKey(5) == nullptr);
+  
    REQUIRE(test.get_count() == 1);
 }
 
@@ -33,9 +34,11 @@ TEST_CASE("insert_leftCh", "[insLeft]") {
    REQUIRE(*test.getLeftKey(5) == 3);
    REQUIRE(test.getRightKey(5) == nullptr);
    REQUIRE(test.getParentKey(5) == nullptr);
+  
    REQUIRE(*test.getParentKey(3) == 5);
    REQUIRE(test.getLeftKey(3) == nullptr);
    REQUIRE(test.getRightKey(3) == nullptr);
+  
    REQUIRE(test.get_count() == 2);
 }
 
@@ -61,8 +64,45 @@ TEST_CASE("insert_rightCh", "[insRight]") {
    REQUIRE(test.getLeftKey(5) == nullptr);
    REQUIRE(*test.getRightKey(5) == 7);
    REQUIRE(test.getParentKey(5) == nullptr);
+  
    REQUIRE(*test.getParentKey(7) == 5);
    REQUIRE(test.getLeftKey(7) == nullptr);
    REQUIRE(test.getRightKey(7) == nullptr);
+  
    REQUIRE(test.get_count() == 2);
+}
+
+
+
+/* insert left child
+  
+       5        ---+3 +7---->       5  
+                                   /  \
+                                 3      7
+
+
+
+*/
+
+TEST_CASE("insert_2_LR_child", "[ins2]") {
+  
+  BST<int> test;
+  
+  test.insert(5);
+  test.insert(3);
+  test.insert(7);
+   REQUIRE(*test.getKeyRoot() == 5);
+   REQUIRE(*test.getLeftKey(5) == 3);
+   REQUIRE(*test.getRightKey(5) == 7);
+   REQUIRE(test.getParentKey(5) == nullptr);
+  
+   REQUIRE(*test.getParentKey(7) == 5);
+   REQUIRE(test.getLeftKey(7) == nullptr);
+   REQUIRE(test.getRightKey(7) == nullptr);
+  
+   REQUIRE(*test.getParentKey(3) == 5);
+   REQUIRE(test.getLeftKey(3) == nullptr);
+   REQUIRE(test.getRightKey(3) == nullptr);
+  
+   REQUIRE(test.get_count() == 3);
 }
