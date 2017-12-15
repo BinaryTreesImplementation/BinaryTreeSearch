bc3f9c15d3357a3f62d1f84b8107ec653877774b
Bozey98 <dmusatov98@gmail.com>
2017-12-15 13:08:32 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,2 +1,14 @@
 #include <BST.hpp>
 #include <catch.hpp>
+
+TEST_CASE("insert_root", "[root]") {
+  
+  BST<int> test;
+  
+  test.insert(5);
+   REQUIRE(*test.getKeyRoot() == 5);
+   REQUIRE(test.getLeftKey(5) == nullptr);
+   REQUIRE(test.getRightKey(5) == nullptr);
+   REQUIRE(test.getParentKey(5) == nullptr);
+   REQUIRE(test.getCount() == 1);
+}
